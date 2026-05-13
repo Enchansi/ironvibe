e30b5a94da768ece099a334ff4678480d9724a28
Enchansi <yousef.inchassi9@gmail.com>
2026-05-13 05:38:07 +0400

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        border: "hsl(240 14% 18%)",
+        background: "hsl(240 14% 5%)",
+        foreground: "hsl(240 10% 92%)",
+      },
+    },
+  },
   plugins: [],
 };
